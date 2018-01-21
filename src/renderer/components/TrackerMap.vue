ff8afcdd08f1bfa5e3b63c75d46cb2b1af439553
Matthew Overall <matthew.overall@boisecodeworks.com>
2018-01-20 17:38:06 -0700

    added overworld items and cycling

--- src/renderer/components/TrackerMap.vue
+++ src/renderer/components/TrackerMap.vue
@@ -19,19 +19,34 @@
         .triangle(@click="l.triforceCollected = !l.triforceCollected" :class="{on:l.triforceCollected}")
           | {{l.level}}
         .heart(@click="l.heartCollected = !l.heartCollected" :class="{on:l.heartCollected}")
-      .item-box(v-for="i in l.items")
-        .item
-          | {{l.item}}
+      .item-box(v-for="item in l.items" @click="cycleItem(item)")
+        .item(:class="items[item.id]")
+    .overworld-items
+      .overworld-item
+        .item-box
+          .item(:class="items[oi.armos.id]" @click="cycleItem(oi.armos)")
+        .item-label A
+      .overworld-item
+        .item-box
+          .item(:class="items[oi.whiteSword.id]" @click="cycleItem(oi.whiteSword)")
+        .item-label WS
+      .overworld-item
+        .item-box
+          .item(:class="items[oi.coast.id]" @click="cycleItem(oi.coast)")
+        .item-label C
 </template>
 
 <script>
 import { mapState } from 'vuex'
+import { createWriteStream } from 'fs';
 
 export default {
   name: 'TrackerMap',
   computed: mapState({
     tiles: state => state.tiles,
-    levels: state => state.tracker.levels
+    levels: state => state.tracker.levels,
+    items: state => state.items,
+    oi: state => state.tracker.overworldItems
   }),
   data () {
     return {
@@ -39,6 +54,15 @@ export default {
       cols: "0123456789ABCDEF",
       selectedCell: "00",
       showArea: false
+    }
+  },
+  methods: {
+    cycleItem (item) {
+      let nextId = item.id + 1
+      if(nextId >= this.items.length){
+        nextId = 0
+      }
+      item.id = nextId
     }
   }
 }
@@ -203,4 +227,50 @@ export default {
     height: 24px
     width: 24px
     border-radius: 4px
+    display: flex
+    align-items: center
+  .overworld-item
+    display: flex
+    flex-direction: row-reverse
+    align-items: center
+  .item-label
+    color: white
+    font-size: 1.5em
+    margin: 0 5px
+  .item
+    height: 20px
+    width: 20px
+    background-image: url('../../../static/items/sprites-items.gif')
+    background-repeat: no-repeat
+    background-position: 100px 100px
+    &.boomerang
+       background-position: -278px 1px
+    &.bow
+       background-position: -317px 2px
+    &.blue-boomerang
+       background-position: -119px -77px
+    &.raft
+       background-position: -318px -78px
+    &.stepladder
+       background-position: -318px -38px
+    &.recorder
+       background-position: -358px -78px
+    &.wand
+       background-position: -198px -78px
+    &.red-candle
+       background-position: -78px -118px
+    &.book
+       background-position: -238px 2px
+    &.magic-key
+       background-position: -158px -78px
+    &.silvers
+       background-position: -38px -118px
+    &.red-ring
+       background-position: -118px -118px
+    &.white-sword
+       background-position: -358px -118px
+    &.power-bracelet
+       background-position: -278px -78px
+    &.heart-container
+       background-position: -237px -37px
 </style>
--- src/renderer/store/index.js
+++ src/renderer/store/index.js
@@ -6,48 +6,54 @@ Vue.use(Vuex)
 
 const state = {
   tiles: tiles,
+  items: ["", "bow", "boomerang", "blue-boomerang", "raft", "stepladder", "recorder", "wand", "red-candle", "red-ring", "book", "magic-key", "white-sword", "heart-container", "power-bracelet"],
   tracker: {
+    overworldItems: {
+      whiteSword: { id: 0 },
+      coast: { id: 0 },
+      armos: { id: 0 },
+    },
     levels: [{
       level: 1,
       triforceCollected: false,
       heartCollected: false,
-      items:["",""]
+      items: [{ id: 0 }, { id: 0 }]
     },
     {
       level: 2,
       triforceCollected: false,
       heartCollected: false,
-      items:[""]
+      items: [{ id: 0 }]
     }, {
       level: 3,
       triforceCollected: false,
       heartCollected: false,
-      items:[""]
+      items: [{ id: 0 }]
     }, {
       level: 4,
       triforceCollected: false,
       heartCollected: false,
-      items:[""]
+      items: [{ id: 0 }]
     }, {
       level: 5,
       triforceCollected: false,
       heartCollected: false,
-      items:[""]
+      items: [{ id: 0 }]
     }, {
       level: 6,
       triforceCollected: false,
       heartCollected: false,
-      items:[""]
+      items: [{ id: 0 }]
     }, {
       level: 7,
       triforceCollected: false,
       heartCollected: false,
-      items:[""]
+      items: [{ id: 0 }]
     }, {
       level: 8,
       triforceCollected: false,
       heartCollected: false,
-      items:[""]
+      items: [{ id: 0 }, { id: 0 }]
     }]
   }
 }
